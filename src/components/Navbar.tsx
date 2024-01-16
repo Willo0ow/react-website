2ceef7515d27160f9265e3e391de6f23eda11af0
Willo0ow <dhinc39@gmail.com>
2024-01-16 21:56:10 +0100

    make navbar absolute

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="absolute text-white w-screen z-10">
         <ul className='flex w-full gap-8'>
             <li className="uppercase hover:text-red-800"><Link to="/" >Home</Link></li>
             <li className="uppercase hover:text-red-800"><Link to="/contact">Contact</Link></li>
