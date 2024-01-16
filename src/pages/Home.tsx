bfe236d824159ecfe4f359a127e6c9ccab6601b9
Willo0ow <dhinc39@gmail.com>
2024-01-16 22:33:51 +0100

    base home services section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/home/Hero";
+import Services from "../components/home/Services";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Services />
     </div>
   );
 };
