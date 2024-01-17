88c02fb516be1456659a7de2a1dcd95d5c6788c3
Dorota <dhinc39@gmail.com>
2024-01-17 09:22:28 +0100

    Base image and text section (#3)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import Hero from "../components/home/Hero";
 import Services from "../components/home/Services";
+import ImageAndTextSection from "../components/ImageAndTextSection";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Services />
+      <ImageAndTextSection imageSide="left" isImageProminent={true} />
+      <ImageAndTextSection imageSide="right" isImageProminent={false} />
     </div>
   );
 };
