097cc31631cbe58729db5b39c75f18fe8bb4329b
Willo0ow <dhinc39@gmail.com>
2024-01-17 15:09:16 +0100

    add responsive styles to image with text section

--- src/components/ImageAndTextSection.tsx
+++ src/components/ImageAndTextSection.tsx
@@ -6,16 +6,19 @@ interface Props {
 
 const ImageAndTextSection = ({ imageSide, isImageProminent }: Props) => {
   return (
-    <section className="flex">
+    <section className="flex flex-col md:flex-row">
       <div className={`${isImageProminent? 'basis-4/6' : 'basis-2/6'} ${imageSide === 'left' ? 'order-1': 'order-2'}`}>
-        <img src={heroImage} alt="random" />
+        <img src={heroImage} alt="random" className={`object-cover w-full h-full ${imageSide === 'left'? 'object-right-top': 'object-left-top'}`} />
       </div>
 
       <div className={`${isImageProminent? 'basis-2/6' : 'basis-4/6'} ${imageSide === 'left' ? 'order-2': 'order-1'} shrink-0 p-8`}>
         <p className="uppercase text-orange-800 font-medium text-sm my-3">Heading</p>
-        <h3 className="text-3xl font-bold my-2">Title</h3>
-        <p className="text-xl">
+        <h3 className="text-xl md:text-3xl font-bold my-2">Title</h3>
+        <p className="text-lg md:text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
           voluptatum.
         </p>
       </div>
